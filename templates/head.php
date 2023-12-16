6eb52a5e2e7b924707545c28ea9064860eff5e26
Suzanne102 <suzanne.chartier1@gmail.com>
2023-12-16 15:54:11 +0300

    Created a testing branch
    Created a testing-es branch so that I can make changes and play here and push to master after

---
+++ templates/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+	<head>
+    	<link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+		<meta charset=UTF-8" />
+		<meta name="author" content="Made by Suzanne Chartier" />
+		<meta name="keywords" content="casa tosca, alquiler vacacional, javea, costa blanca, alojamiento, vacationes, hotel, pueblo antiguo, estudio, alquiler un dia" />
+
+			<!--This would be the text in English-->
+			<!--<meta name="description" content="Casa Tosca, an exquisite boutique apartment rental, offers a unique stay in Javea's medieval town center. This meticulously restored historic townhouse features 4 luxurious apartments and 1 ensuite room, blending rich history with modern comforts. Each air-conditioned apartment in this charming Tosca stone house is thoughtfully designed, boasting high-speed fiber optics internet, elegant wrought iron balconies, large wooden doors, and stone porticoes. Experience the allure of Javea's narrow streets and historical architecture with Casa Tosca's blend of tradition and contemporary amenities."/>
+			-->
+
+		<meta name="description" content="Casa Tosca, un exquisito alquiler de apartamentos tipo boutique, ofrece una estancia única en el centro medieval de Jávea. Esta casa histórica meticulosamente restaurada cuenta con 4 lujosos apartamentos y 1 habitación con baño privado, combinando una rica historia con comodidades modernas. Cada apartamento con aire acondicionado en esta encantadora casa de piedra Tosca está diseñado de manera reflexiva, presumiendo de internet de fibra óptica de alta velocidad, elegantes balcones de hierro forjado, grandes puertas de madera y pórticos de piedra. Experimente el encanto de las estrechas calles y la arquitectura histórica de Jávea con la mezcla de tradición y comodidades contemporáneas de Casa Tosca."/>
+			<!-- InstanceBeginEditable name="doctitle" -->
+<title>CASA TOSCA JAVEA</title>
+
+<!-- InstanceEndEditable -->
+		<link rel="stylesheet" type="text/css" href="../CSS/maincasatosca.css">
+		<script src="../SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+		<link href="../SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+
+		<!-- Google tag (gtag.js) PUT MY ONW TAG AND PROPER GOOGLE ID IN THE JS FILE-->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-G-7MBHPYGQCX"></script>
+		<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-7MBHPYGQCX');
+		</script>
+	</head>
